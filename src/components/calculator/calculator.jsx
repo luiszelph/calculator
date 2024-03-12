5e1fbd513f90593b3a3af8585dd32be384d0c00b
Zelph Moreno <luiszelph.morenoruiz@gmail.com>
2024-03-12 17:51:55 -0600

    Se creo la calculadora

--- src/components/calculator/calculator.jsx
+++ src/components/calculator/calculator.jsx
@@ -1,43 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './calculator.css'
 
-const calculator = () => {
+const Calculator = () => {
+
+  const [value, setValue] = useState('');
+
   return (
     <div className='container'>
         <div className='calculator'>
             <form action="">
-                <div>
-                    <input type='text' />
+                <div className='display'>
+                    <input type='text' value={value} />
                 </div>
                 <div>
-                    <input type='button' value="AC" />
-                    <input type='button' value="DE" />
-                    <input type='button' value="%" />
-                    <input type='button' value="/" />
+                    <input type='button' value="AC" onClick={e => setValue('')}/>
+                    <input type='button' value="DE" onClick={e => setValue(value.slice(0, -1))}/>                
+                    <input type='button' value="." onClick={e => setValue(value + e.target.value)}/>
+                    <input type='button' value="/" onClick={e => setValue(value + e.target.value)}/>
                 </div>
                 <div>
-                    <input type='button' value="7" />
-                    <input type='button' value="8" />
-                    <input type='button' value="9" />
-                    <input type='button' value="*" />
+                    <input type='button' value="7" onClick={e => setValue(value + e.target.value)}/>
+                    <input type='button' value="8" onClick={e => setValue(value + e.target.value)}/>
+                    <input type='button' value="9" onClick={e => setValue(value + e.target.value)}/>
+                    <input type='button' value="*" onClick={e => setValue(value + e.target.value)}/>
                 </div>
                 <div>
-                    <input type='button' value="4" />
-                    <input type='button' value="5" />
-                    <input type='button' value="6" />
-                    <input type='button' value="-" />
+                    <input type='button' value="4" onClick={e => setValue(value + e.target.value)}/>
+                    <input type='button' value="5" onClick={e => setValue(value + e.target.value)}/>
+                    <input type='button' value="6" onClick={e => setValue(value + e.target.value)}/>
+                    <input type='button' value="-" onClick={e => setValue(value + e.target.value)}/>
                 </div>
                 <div>
-                    <input type='button' value="1" />
-                    <input type='button' value="2" />
-                    <input type='button' value="3" />
-                    <input type='button' value="+" />
+                    <input type='button' value="1" onClick={e => setValue(value + e.target.value)}/>
+                    <input type='button' value="2" onClick={e => setValue(value + e.target.value)}/>
+                    <input type='button' value="3" onClick={e => setValue(value + e.target.value)}/>
+                    <input type='button' value="+" onClick={e => setValue(value + e.target.value)}/>
                 </div>
                 <div>
-                    <input type='button' value="00" />
-                    <input type='button' value="0" />
-                    <input type='button' value="." />
-                    <input type='button' value="=" />
+                    <input type='button' value="0" onClick={e => setValue(value + e.target.value)}/>
+                    <input type='button' value="00" onClick={e => setValue(value + e.target.value)}/>                    
+                    <input type='button' value="=" className='equal' onClick={e => setValue(eval(value))}/>
                 </div>
             </form>
         </div>
@@ -45,4 +47,4 @@ const calculator = () => {
   )
 }
 
-export default calculator
+export default Calculator
